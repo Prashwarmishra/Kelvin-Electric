1759c6cfe11ce4adb60526274ff001f0aa87602f
Prashwar Mishra <prashwarmishra@gmail.com>
2021-05-17 20:35:26 +0530

    create nodemailer for sending password reset tokens

---
+++ mailers/password_reset_email_mailer.js
@@ -0,0 +1,20 @@
+const nodemailer = require('../config/nodemailer');
+
+
+exports.passwordResetToken = function(data){
+    let token = data.token;
+    let htmlString = nodemailer.renderTemplate({token: token}, '/password_reset/passwordResetToken.ejs');
+
+    nodemailer.transporter.sendMail({
+        from: 'prod.kelvinelectric@gmail.com',
+        to: token.user.email,
+        subject: 'Reset your Kelvin Electric account password',
+        html: htmlString,
+    }, function(err, info){
+        if(err){
+            console.log('Error in sending password reset email: ', err);
+            return;
+        }
+        console.log(info);
+    })
+}
