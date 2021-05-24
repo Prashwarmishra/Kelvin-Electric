7f11d2eef9be412f27cdd9f37f0523cdd0282823
Prashwar Mishra <prashwarmishra@gmail.com>
2021-05-24 12:36:13 +0530

    design preorder schema

---
+++ models/preorder.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const preorderSchema = new mongoose.Schema({
+    model: {
+        type: String,
+        required: true,
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    shippingState: {
+        type: String,
+        required: true,
+    },
+    shippingCity: {
+        type: String,
+        required: true,
+    },
+    shippingPincode: {
+        type: String,
+        required: true,
+    },
+    shippingDealershipName: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    billingAddress: {
+        type: String,
+        required: true,
+    },
+    billingLandmark: {
+        type: String,
+        required: true, 
+    },
+    billingPincode: {
+        type: String,
+        required: true, 
+    },
+    billingCity: {
+        type: String,
+        required: true,
+    }
+},  {
+        timestamps: true,
+    }
+)
+
+const Preorder = mongoose.model('Preorder', preorderSchema);
+
+module.exports = Preorder;
